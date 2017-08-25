b906cdbbfcce5d34b9f2c8713a548d4f0d59c8c4
mjkhan <mjkhan21@gmail.com>
2017-08-25 10:45:51 +0900

    Role extends AbstractGroup

--- Vortex/src/main/java/vortex/application/role/Role.java
+++ Vortex/src/main/java/vortex/application/role/Role.java
@@ -1,57 +1,5 @@
 package vortex.application.role;
 
-import java.sql.Date;
+import vortex.application.group.AbstractGroup;
 
-public class Role {
-	private String
-		id,
-		name,
-		description,
-		modifiedBy;
-	private Date lastModified;
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
-	
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-	
-	public Date getLastModified() {
-		return lastModified;
-	}
-	
-	public void setLastModified(Date lastModified) {
-		this.lastModified = lastModified;
-	}
-	
-	@Override
-	public String toString() {
-		return "Role{id:'" + id + "', name:'" + name + "'}";
-	}
-}
+public class Role extends AbstractGroup {}

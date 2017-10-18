b02e37b1d709864e14728175efa0c615e8813384
mjkhan <mjkhan21@gmail.com>
2017-10-18 16:32:43 +0900

    HierarchyElement javadoc 주석

--- Vortex/src/main/java/vortex/support/data/hierarchy/HierarchyElement.java
+++ Vortex/src/main/java/vortex/support/data/hierarchy/HierarchyElement.java
@@ -6,67 +6,68 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 
 import vortex.support.AbstractObject;
-/**Interface for objects that are part of and build up a hierarchical structure.<br />
- * A HierarchyElement must provide HierarchyInfo that is used by a HierarchyBuilder in building a hierarchy.
+/**계층 구조를 구성하는 객체들이 구현하는 인터페이스.<br />
+ * HierarchyElement는 HierarchyInfo를 제공한다. HierarchyInfo는 HierarchyBuilder가 계층 구조를 구축하는데 사용한다.
  * @author mjkhan
  */
 public interface HierarchyElement {
-	/**Returns information of the HierarchyElement.
-	 * @return information of the HierarchyElement
+	/**HierarchyElement의 정보를 반환한다.
+	 * @return HierarchyElement 정보
 	 */
 	public HierarchyInfo hierarchyInfo();
-	/**Returns the parent of the element.<br />
-	 * If the element is the root element or the top-most element,<br />
-	 * it may return null or the element itself.
-	 * @return parent of the element
+	/**객체의 상위(부모) 객체를 반환한다.<br />
+	 * 객체가 최상위(루트) 객체거나 여러 최상위 객체들 중 하나일 경우,<br />
+	 * null이나 자기 자신을 반환한다.
+	 * @return 객체의 상위 객체
 	 */
 	public CompositeElement getParent();
-	/**Information on a HierarchyElement that is used by a HierarchyBuilder in creating a hierarchy.<br />
-	 * The ID and parent ID from HierarchyInfo don't have to be actual identifiers of objects.<br />
-	 * They are good enough as long as the HierarchyBuilder can identify objects using the values.
-	 * <p id="top-element">To specify a top HierarchyElement, the parent ID provided in HierarchyInfo should be either
-	 * <ul><li>null</li>
-	 *     <li>equal to the ID returned by the HierarchyInfo</li>
+	/**HierarchyElement의 정보로, HierarchyBuilder가 계층 구조를 구축할 때 사용한다.<br />
+	 * HierarchyInfo의 ID와 parentID는 객체의 실제 식별자일 필요는 없다.<br />
+	 * ID와 parentID는 HierarchyBuilder가 객체들을 구분할 수 있기만 하면 된다.
+	 * <p id="top-element">HierarchyElement가 최상위 객체로 인식되려면, HierarchyInfo의 parentID는
+	 * <ul><li>null, 또는</li>
+	 *     <li>HierarchyInfo의 ID와 같은 값</li>
 	 * </ul>
+	 * 이어야 한다.
 	 * </p>
 	 */
 	public static class HierarchyInfo extends AbstractObject implements Externalizable {
 		private String
 			id,
 			parentID;
-		/**Returns the ID of a HierarchyElement.
-		 * @return ID of a HierarchyElement
+		/**HierarchyElement의 ID를 반환한다.
+		 * @return HierarchyElement의 ID
 		 */
 		public String ID() {
 			return id;
 		}
-		/**Sets the ID of a HierarchyElement.
-		 * @param id ID of a HierarchyElement
-		 * @return the HierarchyInfo
+		/**HierarchyElement의 ID를 설정한다.
+		 * @param id HierarchyElement의 ID
+		 * @return HierarchyInfo
 		 */
 		public HierarchyInfo setID(String id) {
 			this.id = id;
 			return this;
 		}
-		/**Returns the parent ID of a HierarchyElement.
-		 * @return parent ID of a HierarchyElement
+		/**HierarchyElement의 상위 객체의 ID.
+		 * @return HierarchyElement의 상위 객체의 ID
 		 */
 		public String parentID() {
 			return parentID;
 		}
-		/**Sets the parent ID of a HierarchyElement.
-		 * @param parentID parent ID of a HierarchyElement
-		 * @return the HierarchyInfo
+		/**HierarchyElement의 상위 객체의 ID를 설정한다.
+		 * @param parentID HierarchyElement의 상위 객체의 ID
+		 * @return HierarchyInfo
 		 */
 		public HierarchyInfo setParentID(String parentID) {
 			this.parentID = parentID;
 			return this;
 		}
-		/**Returns whether the HierarchyElement is a top element.<br />
-		 * A top element is a HiearchyElement that has no parent element.
+		/**HierarchyElement가 최상위 요소인지 반환한다.<br />
+		 * 최상위 요소란 상위 요소가 없는 HiearchyElement를 말한다.
 		 * @return
-		 * <ul><li>true if the element is at top</li>
-		 * 	   <li>false otherwise</li>
+		 * <ul><li>요소가 최상위이면 true</li>
+		 * 	   <li>그렇지 않으면 false</li>
 		 * </ul>
 		 */
 		public boolean atTop() {

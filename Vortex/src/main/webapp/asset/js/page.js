8551cb67d589f4658d3f442051d4015801934d16
mjkhan <mjkhan21@gmail.com>
2017-11-09 14:01:57 +0900

    paginate 추가

--- Vortex/src/main/webapp/asset/js/page.js
+++ Vortex/src/main/webapp/asset/js/page.js
@@ -158,3 +158,119 @@ var dialog = {
 			dialog.onclose();
 	}
 };
+/**
+ * @param config
+ * {start:0,
+ *  fetchSize:10,
+ *  totalSize:135,
+ *  links:3,
+ *  first:"...",
+ *  previous:"...",
+ *  link:"...",
+ *  current:"...",
+ *  next:"...",
+ *  last:"..."
+ * }
+ * @returns {String}
+ */
+function paginate(config) {
+	var rc = config.totalSize;
+	if (!rc) return "";
+	
+	var	fetchCount = config.fetchSize;
+	if (!fetchCount) return "";
+	
+	var fetch = {
+		all:0,
+		none:-1,
+		count:function(elementCount, size) {
+			if (!elementCount || size == fetch.all) return 1;
+			return parseInt((elementCount / size) + ((elementCount % size) == 0 ? 0 : 1));
+		},
+		end:function(elementCount, size, start) {
+			if (size < fetch.all) throw "Invalid size: " + size;
+			if (elementCount < 0) throw "Invalid elementCount: " + elementCount;
+			var last = elementCount - 1;
+			if (size == fetch.all) return last;
+			return Math.min(last, start + size -1);
+		},
+		page:function(current, count) {
+			return parseInt(count < 1 ? 0 : current / count);
+		},
+		band:function(page, visibleLinks) {
+			return parseInt(visibleLinks < 1 ? 0 : page / visibleLinks);
+		}
+	};
+	var lc = fetch.count(rc, fetchCount);
+	if (lc < 2) return "";
+	
+	var links = ifEmpty(config.links, fetch.all),
+		page = fetch.page(ifEmpty(config.start, 0), fetchCount),
+		band = fetch.band(page, links),
+		tags = {
+			link:function(tag, index, label) {
+				return !tag ? "" : tag(index, label);
+			},
+			first:function() {
+				return band < 2 ? "" : tags.link(config.first, 0);
+			},
+			previous:function() {
+				if (band < 1) return "";
+			    var prevBand = band - 1,
+					prevPage = (prevBand * links) + (links - 1),
+			        fromRec = prevPage * fetchCount;
+			    return tags.link(config.previous, fromRec);
+			},
+			visibleLinks:function() {
+				var s = "",
+					fromPage = links == fetch.all ? 0 : band * links,
+					toPage = links == fetch.all ? lc : Math.min(lc, fromPage + links);
+				for (var i = fromPage; i < toPage; ++i) {
+					if (i == page)
+						s += tags.link(config.current, i, i + 1);
+					else {
+						var fromRec = i * fetchCount;
+						s += tags.link(config.link, fromRec, i + 1);
+					}
+				}
+				return s;
+			},
+			next:function(bandCount) {
+				bandCount = parseInt(bandCount);
+				if (bandCount - band < 2) return "";
+		
+				var nextBand = band + 1,
+					page = nextBand * links,
+					fromRec = page * fetchCount;
+				return tags.link(config.next, fromRec);
+			},
+			last:function(bandCount) {
+				bandCount = parseInt(bandCount);
+			    var lastBand = bandCount - 1;
+			    if (lastBand - band < 2) return "";
+		
+			    var pages = lastBand * links,
+			        fromRec = pages * fetchCount;
+				return tags.link(config.last, fromRec);
+			}
+		},
+		tag = "";
+	if (links != fetch.all) {
+		tag += tags.first();
+		tag += tags.previous();
+	}
+	tag += tags.visibleLinks();
+	if (links != fetch.all) {
+        var bandCount = parseInt(lc / links);
+        bandCount += lc % links == 0 ? 0 : 1;
+		tag += tags.next(bandCount);
+		tag += tags.last(bandCount);
+	}
+	return tag;
+}
+
+$.fn.paginate = function(options) {
+	return this.each(function(){
+		$(this).html(paginate(options));
+	});
+}

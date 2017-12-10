1b1689cf33671699afc9db42a77ac386777fca1a
mjkhan <mjkhan21@gmail.com>
2017-12-11 07:19:33 +0900

    MenuContext.isPermitted 추가

--- Vortex/src/main/java/vortex/application/access/service/Role.java
+++ Vortex/src/main/java/vortex/application/access/service/Role.java
@@ -1,18 +1,11 @@
 package vortex.application.access.service;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.security.core.GrantedAuthority;
 
 import vortex.application.group.AbstractGroup;
 
 public class Role extends AbstractGroup implements GrantedAuthority {
 	private static final long serialVersionUID = 1L;
-	
-	public static boolean contains(List<String> userRoles, List<String> actionRoles) {
-		return !Collections.disjoint(userRoles, actionRoles);
-	}
 
 	@Override
 	public String getAuthority() {
--- Vortex/src/main/java/vortex/application/menu/MenuContext.java
+++ Vortex/src/main/java/vortex/application/menu/MenuContext.java
@@ -4,9 +4,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import vortex.application.User;
+import vortex.support.AbstractObject;
 import vortex.support.data.hierarchy.Hierarchy;
 
-public class MenuContext {
+public class MenuContext extends AbstractObject {
 	private Hierarchy<Menu> menus;
 	private Map<String, List<String>> actionRoles;
 	
@@ -26,9 +28,13 @@ public class MenuContext {
 		this.actionRoles = actionRoles;
 	}
 	
-	public List<String> getPermittedRoles(Menu menu) {
+	private List<String> getRolesFor(Menu menu) {
 		String action = menu.getAction();
-		List<String> roles = actionRoles.get(action);
+		List<String> roles = !isEmpty(action) ? actionRoles.get(action) : null;
 		return roles != null ? roles : Collections.emptyList();
+	}
+	
+	public boolean isPermitted(User user, Menu menu) {
+		return !Collections.disjoint(user.getRoleIDs(), getRolesFor(menu));
 	}
 }
